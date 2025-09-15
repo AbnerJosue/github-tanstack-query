f531431865506fbe5b7f79e754b43031bb70c780
AbnerEstrada <abner.estrada@blautech.com>
2025-09-15 14:34:14 -0600

    Add the project

---
+++ src/issues/actions/get-labels.action.ts
@@ -0,0 +1,11 @@
+import { sleep } from "../../helpers";
+import { githubApi } from "../../api/github.api";
+import { IGithubLabel } from "../interfaces";
+
+
+export const getLabels = async (): Promise<IGithubLabel[]> => {
+    await sleep(1000);
+    const { data } = await githubApi.get<IGithubLabel[]>("/labels");
+    console.log(data);
+    return data;
+}
--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,12 +1,38 @@
+import LoadingSpinner from "../../shared/components/LoadingSpinner";
+import { useLabelQuery } from "../hooks";
+
+
 export const LabelPicker = () => {
+
+  const { labelsQuery } = useLabelQuery();
+
+  if (labelsQuery.isLoading){
+    return(
+      <div className="flex justify-center items-center h-52">
+        <LoadingSpinner />
+      </div>
+    )
+  }
+
+  if( !labelsQuery.data ) {
+    return ( 
+      <div>
+        No se encuentran datos
+      </div>
+    )
+  }
+
+
   return (
-    <>
-      <span
-        className="px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
-        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+    <div className="flex flex-wrap gap-2 justify-center">
+      {labelsQuery.data.map((label) => (
+        <span
+        className="animate-fadeIn px-2 py-1 rounded-full text-xs font-semibold hover:bg-slate-800 cursor-pointer"
+        style={{ border: `1px solid #${label.color}` }}
       >
-        Primary
+        {label.name}
       </span>
-    </>
+      ))}
+    </div>
   );
 };
